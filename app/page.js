c3159d8d6b3c52056b531b3d88f2b89ba6042149
developer-abdulali <ab.ali.soomro@gmail.com>
2023-12-17 22:08:59 +0500

    added react responsive carousel

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+import HeroBanner from "@/Components/HeroBanner/HeroBanner";
 
 export default function Home() {
   return (
     <main>
-      Home...
+      <HeroBanner />
     </main>
-  )
+  );
 }
