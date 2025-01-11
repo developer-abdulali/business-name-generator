098e307a850d074570c7f80a677447b82323cff5
developer-abdulali <ab.ali.soomro@gmail.com>
2025-01-11 11:39:11 +0500

    added changes

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Container from "@/components/Container";
+import HomeBanner from "@/components/HomeBanner";
+
+export default function Home() {
+  return (
+    <div>
+      <Container className="py-10">
+        <HomeBanner />
+      </Container>
+    </div>
+  );
+}
