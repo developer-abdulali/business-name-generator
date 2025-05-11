a9d77c29e396ed544d1bf11076eb455d7cc5199d
Abdul Ali <103456792+developer-abdulali@users.noreply.github.com>
2025-05-11 13:43:35 +0500

    added changes

--- frontend/src/admin/pages/AllAlbums.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AllAlbums = () => {
-  return <div>AllAlbums</div>;
-};
-
-export default AllAlbums;
--- frontend/src/admin/pages/AllSongs.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AllSongs = () => {
-  return <div>AllSongs</div>;
-};
-
-export default AllSongs;
